b358fe97efb10555446b5fc70f8e453e149e28f1
HangorG <gaba93@yandex.ru>
2020-11-24 23:45:20 +0300

    core functionality finished

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const input = document.querySelector("#input-container");
 const countdownForm = document.querySelector("#countdownForm");
 const dateEl = document.querySelector("#date-picker");
+const titleEl = document.querySelector("#title");
 
 const countdownEl = document.querySelector("#countdown");
 const countdownElTitle = document.querySelector("#countdown-title");
 const countdownBtn = document.querySelector("#countdown-button");
 const timeElements = document.querySelectorAll("span");
 
+const completeEl = document.querySelector("#complete");
+const completeElInfo = document.querySelector("#complete-info");
+const completeBtn = document.querySelector("#complete-button");
+
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
+let countdownActive;
 
 // In milisec
 const second = 1000;
@@ -23,22 +29,32 @@ dateEl.setAttribute("min", today);
 
 // Populate countdown / UI
 function updateDOM() {
-  const now = new Date().getTime();
-  const distance = countdownValue - now;
+  countdownActive = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
 
-  const days = Math.floor(distance / day);
-  const hours = Math.floor((distance % day) / hour);
-  const minutes = Math.floor((distance % hour) / minute);
-  const seconds = Math.floor((distance % minute) / second);
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
 
-  countdownElTitle.textContent = `${countdownTitle}`;
-  timeElements[0].textContent = `${days}`;
-  timeElements[1].textContent = `${hours}`;
-  timeElements[2].textContent = `${minutes}`;
-  timeElements[3].textContent = `${seconds}`;
+    input.hidden = true;
 
-  input.hidden = true;
-  countdownEl.hidden = false;
+    if (distance < 0) {
+      countdownEl.hidden = true;
+      clearInterval(countdownActive);
+      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+      completeEl.hidden = false;
+    } else {
+      countdownElTitle.textContent = `${countdownTitle}`;
+      timeElements[0].textContent = `${days}`;
+      timeElements[1].textContent = `${hours}`;
+      timeElements[2].textContent = `${minutes}`;
+      timeElements[3].textContent = `${seconds}`;
+      completeEl.hidden = true;
+      countdownEl.hidden = false;
+    }
+  }, 1000);
 }
 
 function updateCountdown(e) {
@@ -52,5 +68,19 @@ function updateCountdown(e) {
   e.preventDefault();
 }
 
+function reset() {
+  countdownEl.hidden = true;
+  input.hidden = false;
+  completeEl.hidden = true;
+
+  clearInterval(countdownActive);
+  countdownTitle = "";
+  countdownDate = "";
+  dateEl.value = "";
+  titleEl.value = "";
+}
+
 // Event Listeners
 countdownForm.addEventListener("submit", updateCountdown);
+countdownBtn.addEventListener("click", reset);
+completeEl.addEventListener("click", reset);
